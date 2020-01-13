c65d3c5316155b34c075bef7b13c7550bb296aaf
jiyucho9145 <yamatopanda536@gmail.com>
2020-01-13 10:47:40 +0900

    Fixed comments.

--- include/mc/mc_manager.h
+++ include/mc/mc_manager.h
@@ -10,10 +10,10 @@ mc_manager *mc_alloc_manager(void);
 /* Frees an object manager. */
 void mc_free_manager(mc_manager *mg);
 
-/* Creates a managed object. */
+/* Creates an object. */
 mc_object_ptr mc_new_object(mc_manager *mg, mc_size s);
 
-/* Creates a primitive type array. */
+/* Creates an  array. */
 mc_object_ptr mc_new_array(mc_manager *mg, mc_size c, mc_size s);
 
 /* Converts a pointer of an object into a reference of it.*/
@@ -25,5 +25,5 @@ void *mc_get_pointer(mc_manager *mg, mc_size i);
 /* Adds an object to an object manager. */
 void mc_add_object(mc_manager *mg, mc_object_ptr p);
 
-/* Frees an object, print an error message, and exit. */
+/* Frees objects, print an error message, and exit. */
 void *mc_handle_error(mc_manager *mg, mc_error_code code, char message[]);
